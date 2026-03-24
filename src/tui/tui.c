46f2823cdb2148487820355bf98aa72097041c6d
OleksandrSapozhnik <creator.oleksandr@gmail.com>
2026-03-24 22:32:32 +0100

    Format fix

--- src/tui/tui.c
+++ src/tui/tui.c
@@ -19,12 +19,12 @@
 #include "../utils/utils.h"
 
 #include <ctype.h>
-#include <sys/stat.h>
 #include <jansson.h>
 #include <ncurses.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
 
 /* ======================================================================
  * Constants
